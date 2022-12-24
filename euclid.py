bf3cd458c322f4b4b6e94ce638ea154640cbc98f
John Davies <jd.other@hotmail.com>
2022-12-24 15:20:47 +0000

    implemented Euclids GCD recursion algorithm

--- euclid.py
+++ euclid.py
@@ -1,5 +1,14 @@
 def euclid(a, b):
-    # if b == 0
-    #   return a
-    # else return euclid(b, a mod b)
-    pass
+    print(f'euclid({a}, {b})')
+    if b == 0:
+        return a
+    else:
+        return euclid(b, a % b)
+
+# a = 21
+# b = 9
+# a = 1597
+# b = 2584
+a = 1134903170
+b = 1836311903
+print(f'gcd of {a} and {b}: {euclid(a, b)}')
